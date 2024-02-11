7d9f8040d0dd7671da93b8d31b36ee70682541ef
DostonbekMusayev <dostonbekmusayev72@gmail.com>
2024-02-11 14:17:38 +0500

    fixed bugs

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,28 +1,8 @@
-'use client'
+"use client";
 import { useState, useEffect } from "react";
 import { FaRegMoon, FaShoppingCart, FaSun } from "react-icons/fa";
 
-function getFromLocaleStorage(){
-  return localStorage.getItem('theme')||'light'
-}
 function Navbar() {
-  const [theme, setTheme] = useState(getFromLocaleStorage())
-  console.log(theme);
-
-
-  useEffect(() => {
-    localStorage.setItem("theme", theme);
-    const localTheme = localStorage.getItem("theme");
-    document.querySelector("html").setAttribute("data-theme", localTheme);
-  }, [theme]);
-
-  const handleChange = (e) => {
-    if (e.target.checked) {
-      setTheme("light");
-    } else {
-      setTheme("dark");
-    }
-  };
   return (
     <div className="bg-slate-300 dark:bg-slate-800">
       <div className="max-container">
@@ -38,47 +18,10 @@ function Navbar() {
             <li>
               <a href="">Contact</a>
             </li>
-            <div className="flex items-center gap-5">
+
             <div className="indicator">
               <span className="indicator-item badge badge-primary">0</span>
               <FaShoppingCart className="text-2xl" />
-            </div>
-            <label className="swap swap-rotate">
-              {/* this hidden checkbox controls the state */}
-              <input
-                className="outline-none"
-                type="checkbox"
-                onChange={handleChange}
-                checked={theme === "dark" ? false : true}
-              />
-              {/* sun icon */}
-              <svg
-                stroke="currentColor"
-                fill="currentColor"
-                strokeWidth="0"
-                viewBox="0 0 16 16"
-                className="swap-on h-4 w-4"
-                height="1em"
-                width="1em"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"></path>
-              </svg>
-
-              {/* moon icon */}
-              <svg
-                stroke="currentColor"
-                fill="currentColor"
-                strokeWidth="0"
-                viewBox="0 0 16 16"
-                className="swap-off h-4 w-4"
-                height="1em"
-                width="1em"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"></path>
-              </svg>
-            </label>
             </div>
           </ul>
         </div>
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" data-theme='light'>
+    <html lang="en">
       <body className={inter.className}>
         <Navbar/>
         <main>
